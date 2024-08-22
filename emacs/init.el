531a568b88b83132d1b382f237889062a8d8b016
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2024-08-21 23:05:38 -0400

    new emacs init file

--- emacs/init.el
+++ emacs/init.el
@@ -1,44 +1,73 @@
-;; Turn off some unneeded UI elements
+;; Add MELPA to packages
+(require 'package)
+(add-to-list 'package-archives '("MELPA" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;; Spaces, not tabs
+(setq-default indent-tabs-mode nil)
+
+;; Disable menu bar
 (menu-bar-mode -1) 
 
-;; Globally display line numbers
-(global-display-line-numbers-mode 1)
+;; Enable Doom mode line
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+(setq doom-modeline-height 15)
+(setq doom-modeline-icon t)
 
-;; Set line number colors
-(set-face-foreground 'line-number "#D4AF37")
-(set-face-foreground 'line-number-current-line "#FFFFFF")
+;; Set locs for autosave files
+(setq backup-directory-alist
+      `((".*" . "~/.emacs.d/saves")))
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs.d/saves" t)))
+
+;; Enable line numbering globally
+(global-display-line-numbers-mode)
+
+;; Enable auto-completion
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+;; Enable auto-complete mode globally
+(global-auto-complete-mode t)
+;; Set delay before suggestions appear
+(setq ac-auto-show-menu 0.4)
+;; Enable fuzzy matching
+(setq ac-use-fuzzy t)
+;; Add additional sources for completions
+(add-to-list 'ac-sources 'ac-source-dictionary)
+(add-to-list 'ac-sources 'ac-source-words-in-same-mode-buffers)
+;; Customize popup menu appearance
+(set-face-attribute 'ac-candidate-face nil :background "#1a1a1a" :foreground "white")
+(set-face-attribute 'ac-selection-face nil :background "gold" :foreground "black")
+;; Key-binding for auto-complete mode
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+
+;;Enable multiple cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-c C-c") 'mc/edit-lines)
+
+(custom-set-faces
+ '(line-number ((t (:foreground "#5c6370" :background nil :slant italic))))
+ '(line-number-current-line ((t (:foreground "gold" :background nil :weight bold))))
+ '(mode-line ((t (:background "gold" :foreground "black" :box nil))))
+ '(mode-line-inactive ((t (:background "gold" :foreground "black" :box nil)))))
+(custom-set-variables
+ '(custom-enabled-themes `(dracula))
+ '(custom-safe-themes
+   '("c650a74280e8ce4ae4b50835b7a3bc62aeffa202ffea82260e529f0a69027696" default))
+ '(package-selected-packages
+   '(dracula-theme multiple-cursors moody doom-modeline auto-complete all-the-icons)))
+(set-face-background 'default "unspecified-bg")
 
 ;; Set ToDo states and colors
 (setq org-todo-keywords
-      '((sequence "TODO" "IN-PROG" "WAITING" "|" "DONE")))
+      '((sequence "TODO" "IN-PROG" "WAITING" "|" "DONE" "MIGRATED")))
 
 (setq org-todo-keyword-faces
-      '(("TODO" . org-warning) ("IN-PROG" . "orange")
-        ("WAITING" . "yellow")))
+      '(("TODO" . "red") ("IN-PROG" . "orange")
+        ("WAITING" . "yellow") ("DONE" . "green")("MIGRATED" . "light blue")))
 
 ;; Add appropriate key-bindings and files to org-agenda
 (global-set-key (kbd "C-c a") #'org-agenda)
 (setq org-agenda-files '("~/Workflow/ToDo"))
-
-;; Save all emacs auto-saves in spec dir 
-(setq backup-directory-alist
-      `((".*" . "~/.auto-save")))
-(setq auto-save-file-name-transforms
-      `((".*" "~/.auto-save" t)))
-
-;; Add MELPA to list of package archives
-(require 'package)
-(add-to-list 'package-archives 
-    '("MELPA" .
-      "http://melpa.org/packages/"))
-(package-initialize)
-
-;; Add and load Github Dark theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'github-dark-vscode t)
-
-;;Remove theme background
-(defun on-after-init ()
-  (unless (display-graphic-p (selected-frame))
-    (set-face-background 'default "unspecified-bg" (selected-frame))))
-(add-hook 'window-setup-hook 'on-after-init)
